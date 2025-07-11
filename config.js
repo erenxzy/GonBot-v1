5ba03856e876fc9ca8980bbc7c0ba4df648e5e87
ᴇʀᴇɴxsxʏ <diegolesimarcelino@gmail.com>
2025-07-11 18:38:07 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -24,14 +24,14 @@ global.packname = 'ɢᴏɴ-ᴠ1'
 global.namebot = 'ɢᴏɴʙᴏᴛ-ᴠ1'
 global.author = '© ᴍᴀᴅᴇ ʙʏ ᴘʀᴏʏᴇᴄᴛ ɢᴏɴ-ᴠ1'
 global.moneda = 'Dolar'
-global.canalreg = '120363402895449162@newsletter'
+global.canalreg = '120363417252896376@newsletter'
 
 global.namecanal = 'Gᴏɴ-ʙᴏᴛᴠ1 ᴘʀᴏʏᴇᴄᴛ '
 global.canal = 'https://whatsapp.com/channel/0029Vb5pM031CYoMvQi2I02D'
-global.idcanal = '120363403739366547@newsletter'
+global.idcanal = '120363417252896376@newsletter'
 
 global.ch = {
-ch1: '120363420941524030@newsletter',
+ch1: '120363417252896376@newsletter',
 }
 
 global.multiplier = 69 
