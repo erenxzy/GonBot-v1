5b0ceb147a37d837c1925ec89d6b8c003172056a
ᴇʀᴇɴxsxʏ <diegolesimarcelino@gmail.com>
2025-07-11 23:04:51 -0400

    Actualizar anime-info.js

--- plugins/anime-info.js
+++ plugins/anime-info.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `💣 *Ingrese el nombre de algun anime*\n\nEjemplo, ${usedPrefix + command} Megumin`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🌟 *Ingrese el nombre de algun anime*\n\nEjemplo, ${usedPrefix + command} One piece`, m, rcanal)
 let res = await fetch('https://api.jikan.moe/v4/manga?q=' + text)
 if (!res.ok) return conn.reply(m.chat, `❌ *Ocurrió un fallo*`, m, rcanal)
 
